bb4746de5fe00944eb878be3f9ae12c0e8e619f1
sjulka-zscaler <sjulka@zscaler.com>
2026-06-17 13:44:45 +0530

    ZS-53208: Upgrade Terraform AWS provider to v6.x to support new AWS regions (#8)
    * ZS-53208: Upgrade Terraform AWS provider to v6.x to support new AWS regions
    
    * ZS-53208: Update READMEs to reflect AWS provider v6.x and Terraform >= 1.5.0
    
    * ZS-53208: Revert accidental aws_region change in vzen_standalone tfvars

--- modules/terraform-zsvzen-vm-aws/versions.tf
+++ modules/terraform-zsvzen-vm-aws/versions.tf
@@ -2,16 +2,16 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.32"
+      version = "~> 6.0"
     }
     local = {
       source  = "hashicorp/local"
-      version = "~> 2.2.0"
+      version = "~> 2.5"
     }
     null = {
       source  = "hashicorp/null"
-      version = "~> 3.1.0"
+      version = "~> 3.2"
     }
   }
-  required_version = ">= 0.13.7, < 2.0.0"
+  required_version = ">= 1.5.0, < 2.0.0"
 }
